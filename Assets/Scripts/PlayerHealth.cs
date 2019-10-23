9a27824fe8aac28121eea642aeb9e584138b9111
Mauro <iviauricius@gmail.com>
2019-10-23 17:22:01 -0500

    Mauro - Player Health Scripts Added
    Added a the player health script. Will test collisions with other colliders soon.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (health == 0.0f)
+            health = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,11 @@ using Rewired;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1.0f;
-    public int reWiredId = 0;
     // Player's input keys.
     public string up;
     public string down;
     public string left;
     public string right;
-    // Rewired object.
-    private Player player;
 
     // Awake is called as soon as the object is created.
     private void Awake()
@@ -71,24 +68,6 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Assign the new position to the player.
-        transform.position = tempPosition;
-    }
-    // ReWired get input.
-    void ReWiredInput()
-    {
-        Vector3 tempPosition = transform.position;
-        float xMove = player.GetAxis("MoveHorizontal");
-        float yMove = player.GetAxis("MoveVertical");
-        if(xMove != 0.0f)
-        {
-            print("ReWired Joystick " + reWiredId + " - Horizontal");
-            tempPosition.x += xMove * speed * Time.deltaTime;
-        }
-        if(xMove != 0.0f)
-        {
-            print("ReWired Joystick " + reWiredId + " - Vertical");
-            tempPosition.y += yMove * speed * Time.deltaTime;
-        }
         transform.position = tempPosition;
     }
 }
