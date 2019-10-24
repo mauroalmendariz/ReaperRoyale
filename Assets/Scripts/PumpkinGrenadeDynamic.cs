5a6600f2875eaebb7532722339084630277df2ae
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-10-24 06:40:18 -0500

    Added Script for Pumpkin Grenade (Dynamic Rigidbody2D)

---
+++ Assets/Scripts/PumpkinGrenadeDynamic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinGrenadeDynamic : MonoBehaviour
+{
+    public float launchForce;
+
+    private Rigidbody2D rigBody;
+
+    private void OnEnable()
+    {
+        rigBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Vector2 force = (new Vector2(1, 1)).normalized * launchForce;
+
+        rigBody.AddForce(force);
+    }
+}
